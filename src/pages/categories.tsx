b4ff35e2cb0cf6e8ab2c864edc4c292ef441817e
Samson Akinfenwa <akinfenwasamson@gmail.com>
2023-08-19 16:35:14 +0100

    First commit

---
+++ src/pages/categories.tsx
@@ -0,0 +1,12 @@
+import React, {useState,useEffect} from 'react';
+
+type CategoriesProps = {}
+
+const Categories: React.FC<CategoriesProps> = (props) => {
+    const {} = props;
+    return (
+        <></>
+    );
+};
+
+export default Categories;
---
+++ src/pages/groups.tsx
@@ -0,0 +1,13 @@
+import React, {useState,useEffect} from 'react';
+
+type GroupsProps = {}
+
+const Groups: React.FC<GroupsProps> = (props) => {
+    const {} = props;
+    return (
+        <>
+        </>
+    );
+};
+
+export default Groups;
