edf0a1207f8432c1137ffa03bdc3e3494c599a38
Samson Akinfenwa <akinfenwasamson@gmail.com>
2023-09-09 22:44:04 +0100

    First commit

--- src/pages/event/[id]/reviews.tsx
+++ src/pages/event/[id]/reviews.tsx
@@ -1,33 +1,44 @@
 import React, {useState,useEffect} from 'react';
 import Layout from "@/components/layouts";
-import {Svg, useQuery} from "@/components/rn-alpha";
+import {Loader, Svg, useQuery} from "@/components/rn-alpha";
 import {star} from "@/utils/icons/icons";
 import PATHS from "@/paths";
+import {useRouter} from "next/router";
 
 type ReviewsProps = {}
 
 const Reviews: React.FC<ReviewsProps> = (props) => {
     const {} = props;
-	const {loading,error,data} = useQuery(PATHS.reviews)
+	const router = useRouter();
+	const {loading,error,data} = useQuery(PATHS.reviews, {variables:{eventId: router.query.id}, networkPolicy:"network-and-cache"})
 
     return (
         <Layout>
-	        <div className="container bg max-w-3xl">
+	        <div className="container max-w-3xl">
+		        <h1 className="text-2xl">Reviews</h1>
 		        {data?.map((item:any)=>(
-			        <div className="">
+			        <div className="mt-5 bg p-5">
 				        <div className="flex-item gap-4">
-					        <p className="font-bold text-lg">{item.user.name}</p>
+					        <div className="w-10 h-10 bg-shade rounded-full"/>
+					        <p className="font-bold text-base -ml-2">{item.user.name}</p>
 					        <div className="flex-item gap-2">
 						        <Svg icon={star} className="w-5 text-primary"/>
 						        <p>{item.rate}</p>
 					        </div>
 				        </div>
-				        <p className="text-base text mt-4">{item.comment}</p>
+				        <p className="text-base text mt-2">{item.comment}</p>
 			        </div>
 		        ))}
+		        <Loader loading={(loading && (!data || data?.length < 1))}/>
 	        </div>
         </Layout>
     );
 };
 
 export default Reviews;
+
+export async function getServerSideProps({query}:any) {
+	return {
+		props: { query }
+	}
+}
