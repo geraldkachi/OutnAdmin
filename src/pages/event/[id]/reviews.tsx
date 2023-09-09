ee8245d2373f37152f997ec72a7072d57fc0cfc7
Samson Akinfenwa <akinfenwasamson@gmail.com>
2023-09-09 20:46:17 +0100

    First commit

---
+++ src/pages/event/[id]/reviews.tsx
@@ -0,0 +1,15 @@
+import React, {useState,useEffect} from 'react';
+import Layout from "@/components/layouts";
+
+type ReviewsProps = {}
+
+const Reviews: React.FC<ReviewsProps> = (props) => {
+    const {} = props;
+    return (
+        <Layout>
+
+        </Layout>
+    );
+};
+
+export default Reviews;
